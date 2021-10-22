43ad414c7e2079dbc5564ba8f0821792dabc9929
sharanya-professional <92979051+sharanya-professional@users.noreply.github.com>
2021-10-22 14:54:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup() {  
+  createCanvas(700,900);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+   background("purple");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
